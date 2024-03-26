d0085a6a7fb1e5a9e40ed26e58cd7fc4cd10292a
repozen <111631956+repozen@users.noreply.github.com>
2024-03-26 22:27:28 +0200

    Add files via upload

---
+++ GameTally/GameTally/Enums/UserStatus.cs
@@ -0,0 +1,25 @@
+﻿namespace GameTally.Enums
+{
+    enum UserStatus
+    {
+        NONE = 0,
+        OFFLINE = 1,
+        IN_GAME = 2,
+    }
+
+    public class Convertor
+    {
+        public static string UserStatusConvertor(int userStatus)
+        {
+            switch (userStatus)
+            {
+                case (int)UserStatus.IN_GAME:
+                    return "Plays ";
+                case (int)UserStatus.OFFLINE:
+                    return "Offline";
+            }
+
+            return null;
+        }   
+    }
+}
---
+++ GameTally/GameTally/Models/MenuStripStyle.cs
@@ -0,0 +1,65 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace GameTally.Models
+{
+    public class MenuStripStyle : ProfessionalColorTable
+    {
+        public override Color MenuItemSelected
+        {
+            get { return Color.FromArgb(45, 45, 45); }
+        }
+        public override Color MenuItemBorder
+        {
+            get { return Color.FromArgb(30, 30, 30); }
+        }
+        public override Color MenuBorder
+        {
+            get { return Color.FromArgb(30, 30, 30); }
+        }
+
+        public override Color MenuItemPressedGradientBegin
+        {
+            get { return Color.FromArgb(45, 45, 45); }
+        }
+        public override Color MenuItemPressedGradientEnd
+        {
+            get { return Color.FromArgb(45, 45, 45); }
+        }
+
+        public override Color ToolStripBorder
+        {
+            get { return ColorTranslator.FromHtml("#4C4A48"); }
+        }
+
+        public override Color MenuItemSelectedGradientBegin
+        {
+            get { return Color.FromArgb(45, 45, 45); }
+        }
+
+        public override Color MenuItemSelectedGradientEnd
+        {
+            get { return Color.FromArgb(45, 45, 45); }
+        }
+
+        public override Color ToolStripDropDownBackground
+        {
+            get { return ColorTranslator.FromHtml("#404040"); }
+        }
+
+        public override Color ToolStripGradientBegin
+        {
+            get { return ColorTranslator.FromHtml("#404040"); }
+        }
+
+        public override Color ToolStripGradientEnd
+        {
+            get { return ColorTranslator.FromHtml("#404040"); }
+        }
+
+        public override Color ToolStripGradientMiddle
+        {
+            get { return ColorTranslator.FromHtml("#404040"); }
+        }
+    }
+}
